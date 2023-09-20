b5ed50a08d69482d85066ffd13965ae39ac610d8
Brooks Wimer <bwimer@bu.edu>
2023-09-20 18:53:04 -0400

    edited my function to only use class lib functions

--- assigns/assign0/MySolution/assign0-4.ml
+++ assigns/assign0/MySolution/assign0-4.ml
@@ -6,16 +6,16 @@
 
 
 let str2int cs =
-  let rec aux acc index =
+  let rec aux acc index position_val =
     if index < 0 then
       acc
     else
-      let digit = Char.code cs.[index] - Char.code '0' in
-      let new_acc = acc + (digit * int_of_float (10.0 ** float_of_int (String.length cs - index - 1))) in
-      aux new_acc (index - 1)
+      let digit = ord cs.[index] - ord '0' in
+      let new_acc = acc + (digit * position_val) in
+      aux new_acc (index - 1) (position_val*10)
   in
-  if String.length cs = 0 then
+  if string_length cs = 0 then
     0
   else
-    aux 0 (String.length cs - 1)
+    aux 0 (string_length cs - 1) (1)
 ;;
