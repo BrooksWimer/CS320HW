39c8c7be556c4f9db53f65695791cfa9cb609bdc
Brooks Wimer <bwimer@bu.edu>
2023-09-20 18:34:53 -0400

    I updated this file to only incorporate class lib functions

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -2,8 +2,26 @@
 
 #use "./../assign0.ml";;
 
+#use "./../MyOCaml.ml";;
+
 (* ****** ****** *)
 
 let int2str i0 =
-  string_of_int i0
+  let res = ref "" in
+  let rec helper n =
+    if n < 0 then begin
+      let x = char_of_digit(n) in
+      res := string_cons(x)(!res);
+      helper (-n)
+    end
+    else if n < 10 then
+      res := string_cons(char_of_digit(n))(!res)
+    else begin
+      res := string_cons(char_of_digit(n mod 10))(!res);
+      helper (n / 10);
+    end
+  in
+  helper i0;
+  !res
+
 ;;
