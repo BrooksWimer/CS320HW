cf481d9c097718420a7ff6c7896f5a472c1aff41
Brooks Wimer <bwimer@bu.edu>
2023-09-20 22:53:24 -0400

    I added comments to this code

--- assigns/assign1/MySolution/assign1-1.ml
+++ assigns/assign1/MySolution/assign1-1.ml
@@ -4,19 +4,27 @@
 
 (* ****** ****** *)
 
+(*
+  Function to reverse the digits of an integer while preserving its sign.
+  Parameters:
+    - n: The integer to be reversed.
+  Returns:
+    - The integer with reversed digits.
+*)
 let intrev10 n =
-  let rec aux acc n = 
+  (* Define a recursive auxiliary function to reverse the digits. *)
+  let rec aux acc n =
     if n = 0 then
-      acc
-    else 
-      let digit = n mod 10 in
-      let new_acc = acc * 10 + digit in
-      aux new_acc (n / 10)
+      acc  (* If n is fully processed, return the reversed integer. *)
+    else
+      let digit = n mod 10 in  (* Extract the last digit of n. *)
+      let new_acc = acc * 10 + digit in  (* Append the digit to the reversed result. *)
+      aux new_acc (n / 10)  (* Recursively process the remaining digits. *)
   in
   if n < 0 then
-    -aux 0 (-n) 
+    -aux 0 (-n)  (* If n is negative, reverse its absolute value and negate the result. *)
   else if n = 0 then
-    0
-  else 
-    aux 0 n
+    0  (* If n is 0, return 0 as it remains unchanged when reversed. *)
+  else
+    aux 0 n  (* If n is positive, reverse its digits. *)
 ;;
--- assigns/assign1/MySolution/assign1-2.ml
+++ assigns/assign1/MySolution/assign1-2.ml
@@ -1,34 +1,38 @@
-(* ****** ****** *)
-
 #use "./../assign1.ml";;
 
 #use "./../MyOCaml.ml";;
 
-(* ****** ****** *)
+(*
+  Merge two strings while preserving the order of characters.
+  Parameters:
+    - cs1: The first input string.
+    - cs2: The second input string.
+  Returns:
+    - The merged string with characters in non-decreasing order.
+*)
+let merge_strings(cs1)(cs2) =
+  let l1 = string_length cs1 in
+  let l2 = string_length cs2 in
 
-let string_merge(cs1) (cs2)=
-    let l1 = string_length cs1 in
-    let l2 = string_length cs1 in
-    let rec aux index1 index2 work =
-        if (index1 = l1) then
-          int1_foreach(l2-index2) (fun newIndex-> work(string_get_at(cs2)(index2 + newIndex)))
-        else if (index2 = l2) then 
-          int1_foreach(l1-index1) (fun newIndex -> work(string_get_at(cs1)(index1+newIndex)))
-        
-        else
-          let char1 = string_get_at(cs1)(index1) in
-          let char2 = string_get_at(cs2)(index2) in
+  (* Define a recursive auxiliary function for merging. *)
+  let rec merge_helper index1 index2 work =
+    if index1 = l1 then
+      (* If cs1 is fully processed, append remaining characters from cs2. *)
+      int1_foreach (l2 - index2) (fun newIndex -> work (string_get_at cs2 (index2 + newIndex)))
+    else if index2 = l2 then
+      (* If cs2 is fully processed, append remaining characters from cs1. *)
+      int1_foreach (l1 - index1) (fun newIndex -> work (string_get_at cs1 (index1 + newIndex)))
+    else
+      let char1 = string_get_at cs1 index1 in
+      let char2 = string_get_at cs2 index2 in
 
-          if char1 < char2 then
-            (work(char1); aux(index1+1)(index2)(work))
-          else 
-            (work(char2); aux(index1)(index2+1)(work))
-    in
-    string_make_fwork(aux(0)(0))
-
+      if char1 < char2 then
+        (* Append the smaller character from cs1 and move to the next index. *)
+        (work char1; merge_helper (index1 + 1) index2 work)
+      else
+        (* Append the smaller character from cs2 and move to the next index. *)
+        (work char2; merge_helper index1 (index2 + 1) work)
+  in
+  (* Start the merging process with initial indices. *)
+  string_make_fwork (merge_helper 0 0)
 ;;
-    
-
-
-  
-
